138a242fb9ec4dcc8fa03ca6799fc6a81a26e261
Nathaniel Akubuo <nakubuo@gmail.com>
2025-12-23 13:52:04 +0100

    initial colours

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -7,3 +7,8 @@ const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
 const Color kcBackgroundColor = kcDarkGreyColor;
+
+const Color kTextColor = kFDFDFD;
+
+const Color k343230 = Color(0xFF343230);
+const Color kFDFDFD = Color(0xFFFDFDFD);
