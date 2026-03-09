28278cffbb69576634646122479cf3c9b4a47a18
Nathaniel Akubuo <nakubuo@gmail.com>
2026-03-09 11:25:51 +0100

    splashscreen placeholder

--- lib/ui/views/startup/startup_view.dart
+++ lib/ui/views/startup/startup_view.dart
@@ -1,3 +1,4 @@
+import 'package:chess_app/ui/widgets/images/image_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:stacked/stacked.dart';
@@ -8,13 +9,7 @@ class StartupView extends StackedView<StartupViewModel> {
   const StartupView({Key? key}) : super(key: key);
 
   @override
-  Widget builder(
-    BuildContext context,
-    StartupViewModel viewModel,
-    Widget? child,
-  ) {
-    return const Scaffold();
-  }
+  Widget builder(BuildContext context, StartupViewModel viewModel, Widget? child) => const _View();
 
   @override
   StartupViewModel viewModelBuilder(BuildContext context) => StartupViewModel();
@@ -22,4 +17,109 @@ class StartupView extends StackedView<StartupViewModel> {
   @override
   void onViewModelReady(StartupViewModel viewModel) =>
       SchedulerBinding.instance.addPostFrameCallback((timeStamp) => viewModel.runStartupLogic());
+}
+
+class _View extends StatefulWidget {
+  const _View();
+
+  @override
+  State<_View> createState() => _ViewState();
+}
+
+class _ViewState extends State<_View> with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  late final Animation<Offset> _knightOffset;
+  late final Animation<double> _knightOpacity;
+  late final Animation<double> _logoOpacity;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1200),
+    );
+
+    _knightOffset = TweenSequence<Offset>([
+      TweenSequenceItem(
+        tween: Tween(
+          begin: const Offset(-0.3, 0.3),
+          end: const Offset(-0.3, -0.3),
+        ),
+        weight: 50,
+      ),
+      TweenSequenceItem(
+        tween: Tween(
+          begin: const Offset(-0.3, -0.3),
+          end: const Offset(0.3, -0.3),
+        ),
+        weight: 50,
+      ),
+    ]).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.25, 0.7, curve: Curves.easeOut),
+      ),
+    );
+
+    _knightOpacity = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.25, 0.7, curve: Curves.easeIn),
+    );
+
+    _logoOpacity = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0.7, 1.0, curve: Curves.easeIn),
+    );
+
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnimatedBuilder(
+        animation: _controller,
+        builder: (_, __) {
+          return Stack(
+            alignment: Alignment.center,
+            children: [
+              const SizedBox.expand(),
+              SlideTransition(
+                position: _knightOffset,
+                child: Opacity(
+                  opacity: _knightOpacity.value,
+                  child: const _KnightIcon(),
+                ),
+              ),
+              Opacity(
+                opacity: _logoOpacity.value,
+                child: const ImageCard.local('na8.svg', width: 140),
+              ),
+            ],
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _KnightIcon extends StatelessWidget {
+  const _KnightIcon();
+
+  @override
+  Widget build(BuildContext context) {
+    return const ImageCard.local(
+      'knight-white.svg',
+      size: 72,
+    );
+  }
 }
--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,3 +1,4 @@
+import 'package:chess_app/ui/common/app_values.dart';
 import 'package:stacked/stacked.dart';
 import 'package:chess_app/app/app.locator.dart';
 import 'package:chess_app/app/app.router.dart';
@@ -8,7 +9,7 @@ class StartupViewModel extends BaseViewModel {
 
   // Place anything here that needs to happen before we get into the application
   Future runStartupLogic() async {
-    await Future.delayed(Duration.zero);
+    await Future.delayed(twoSeconds);
 
     // This is where you can make decisions on where your app should navigate when
     // you have custom startup logic
